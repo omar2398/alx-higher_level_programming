931bbf9d091ab3b6210693eb7e78e71a9878d950
Omar mohamed <118669490+omar2398@users.noreply.github.com>
2023-09-28 17:20:17 +0300

    Delete 0x03-python-data_structures directory

--- 0x03-python-data_structures/5-no_c.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def no_c(my_string):
-    listnew = []
-    for i in my_string:
-        if i != 'c' and i != 'C':
-            listnew.append(i)
-    return ''.join(listnew)
--- 0x03-python-data_structures/8-multiple_returns.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-def multiple_returns(sentence):
-    if not sentence:
-        return (len(sentence), None)
-    else:
-        return (len(sentence), sentence[0])
