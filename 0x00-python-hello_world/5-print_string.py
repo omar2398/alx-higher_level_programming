05fd7b00291e8c9714697f2b198fd3d88116fcce
OmAR <omarsteven2398@gmail.com>
2023-09-28 17:04:01 +0300

    omar

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 str = "Holberton School"
+<<<<<<< Updated upstream
 print(3 * str)
 print(str[0:9])
+=======
+print("{}".format(str) * 3)
+print("{}".format(str[0:9]))
+>>>>>>> Stashed changes
