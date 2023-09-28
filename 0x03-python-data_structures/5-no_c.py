3be8b8df1c2786fdc55f724df26e89e029186cb3
Omar mohamed <118669490+omar2398@users.noreply.github.com>
2023-09-28 21:34:00 +0300

    Delete 0x03-python-data_structures directory

--- 0x03-python-data_structures/5-no_c.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def no_c(my_string):
-    new_string = my_string.translate({ord('c'): None, ord('C'): None})
-    return (new_string)
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    for mat in matrix:
-        print(" ".join("{:d}".format(idx) for idx in mat))
