c6e446071d725cc55a8304d64ad49e30359b2284
Omar mohamed <118669490+omar2398@users.noreply.github.com>
2023-09-28 17:10:05 +0300

    Delete 0x01-python-if_else_loops_functions directory

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-for letra in range(ord('a'), ord('z') + 1):
-    print('{:c}'.format(letra), end='')
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-for letra in range(ord('a'), ord('z') + 1):
-    if letra != ord('q') and letra != ord('e'):
-        print('{:c}'.format(letra), end='')
